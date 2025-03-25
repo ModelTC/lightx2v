38d0a2050302fb0e94442041f9fec69c36933e2e
“de1star” <843414674@qq.com>
2025-03-25 16:13:32 +0800

    parallel attention

--- run_hunyuan_t2v_dist.sh
+++ run_hunyuan_t2v_dist.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-
 # model_path=/mnt/nvme1/yongyang/models/hy/ckpts # H800-13
 # model_path=/mnt/nvme0/yongyang/projects/hy/HunyuanVideo/ckpts # H800-14
 model_path=/workspace/ckpts_link # H800-14
 
-export CUDA_VISIBLE_DEVICES=2
-python main.py \
+
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+torchrun --nproc_per_node=4 main.py \
 --model_cls hunyuan \
 --model_path $model_path \
 --prompt "A cat walks on the grass, realistic style." \
@@ -14,5 +13,5 @@ python main.py \
 --target_height 720 \
 --target_width 1280 \
 --attention_type flash_attn2 \
---save_video_path ./output_lightx2v_int8.mp4 \
---mm_config '{"mm_type": "W-int8-channel-sym-A-int8-channel-sym-dynamic-Vllm", "weight_auto_quant": true}' 
+--mm_config '{"mm_type": "W-int8-channel-sym-A-int8-channel-sym-dynamic-Vllm", "weight_auto_quant": true}' \
+--parallel_attn
