3621717e4c9aef654b62c8288de2f1ed92108546
gushiqiao <gushiqiao@sensetime.com>
2025-04-03 17:00:43 +0800

    ring attn

--- lightx2v/attentions/distributed/ring/attn.py
+++ lightx2v/attentions/distributed/ring/attn.py
@@ -102,7 +102,7 @@ def ring_attn_sub(q, k, v,
 
 def ring_attn(q, k, v, img_qkv_len, cu_seqlens_qkv, attention_type="flash_attn2"):
     """
-    执行 Ulysses 注意力机制，结合图像和文本的查询、键和值。
+    执行 Ring 注意力机制，结合图像和文本的查询、键和值。
 
     参数:
         q (torch.Tensor): 查询张量，形状为 [shard_seqlen, heads, hidden_dims]
@@ -120,7 +120,16 @@ def ring_attn(q, k, v, img_qkv_len, cu_seqlens_qkv, attention_type="flash_attn2"
     cur_rank = dist.get_rank()
     world_size = dist.get_world_size()
     
-    shard_seqlen = img_qkv_len
+    # if cur_rank == 0:
+    #     import pdb; pdb.set_trace()
+    # import time; time.sleep(9999)
+    
+    if len(cu_seqlens_qkv) == 3:
+        txt_qkv_len = cu_seqlens_qkv[1] - img_qkv_len  # 文本查询、键和值的长度
+        txt_mask_len = cu_seqlens_qkv[2] - img_qkv_len  # 文本掩码长度
+    elif len(cu_seqlens_qkv) == 2:
+        txt_qkv_len = cu_seqlens_qkv[1] - img_qkv_len  # 文本查询、键和值的长度
+        txt_mask_len = 0
 
     # if RING_COMM is None:
     #     init_ring_comm()
@@ -138,10 +147,12 @@ def ring_attn(q, k, v, img_qkv_len, cu_seqlens_qkv, attention_type="flash_attn2"
     v = v.unsqueeze(0)
 
     img_q, img_k, img_v = q[:,:img_qkv_len,:,:].contiguous(), k[:,:img_qkv_len,:,:].contiguous(), v[:,:img_qkv_len,:,:].contiguous()
-    txt_q, txt_k, txt_v = q[:,img_qkv_len:,:,:].contiguous(), k[:,img_qkv_len:,:,:].contiguous(), v[:,img_qkv_len:,:,:].contiguous()
+    txt_q, txt_k, txt_v = q[:,img_qkv_len:img_qkv_len+txt_qkv_len,:,:].contiguous(), k[:,img_qkv_len:img_qkv_len+txt_qkv_len,:,:].contiguous(), v[:,img_qkv_len:img_qkv_len+txt_qkv_len,:,:].contiguous()
 
     out, lse, next_k, next_v = None, None, None, None
 
+    if len(cu_seqlens_qkv) == 3:
+        q = torch.cat((img_q, txt_q), dim=1)
     k = img_k
     v = img_v
     
@@ -154,16 +165,9 @@ def ring_attn(q, k, v, img_qkv_len, cu_seqlens_qkv, attention_type="flash_attn2"
         if step + 1 == world_size:
             k = torch.cat((k, txt_k), dim=1)
             v = torch.cat((v, txt_v), dim=1)
-        # else:
-        # k = img_k
-        # v = img_v
 
-        # try:
         block_out, block_lse = ring_attn_sub(q, k, v)
-        # except Exception as e:
-        #     if cur_rank == 0:
-        #         import pdb; pdb.set_trace()
-        #     import time; time.sleep(999)
+
         out, lse = update_out_and_lse(out, lse, block_out, block_lse)
 
         if step + 1 != world_size:
@@ -171,10 +175,24 @@ def ring_attn(q, k, v, img_qkv_len, cu_seqlens_qkv, attention_type="flash_attn2"
             k = next_k
             v = next_v
 
-    attn = out.to(torch.bfloat16).squeeze(0).reshape(shard_seqlen, -1)
+    attn1 = out.to(torch.bfloat16).squeeze(0).reshape(img_qkv_len+txt_qkv_len, -1)
     
-    # if cur_rank == 0:
-    #     import pdb; pdb.set_trace()
-    # import time; time.sleep(999)
+    if txt_mask_len > 0:
+        attn2, *_ = _flash_attn_forward(
+            q[:,-(txt_mask_len - txt_qkv_len):,:,:].contiguous(),
+            k[:,-(txt_mask_len - txt_qkv_len):,:,:].contiguous(),
+            v[:,-(txt_mask_len - txt_qkv_len):,:,:].contiguous(),
+            dropout_p=0.0,
+            softmax_scale=q.shape[-1] ** (-0.5),
+            causal=False,
+            window_size_left=-1,
+            window_size_right=-1,
+            softcap=0.0,
+            alibi_slopes=None,
+            return_softmax=False,
+        )
 
-    return attn
+        attn2 = attn2.to(torch.bfloat16).squeeze(0).reshape((txt_mask_len - txt_qkv_len), -1)
+        attn1 = torch.cat([attn1, attn2], dim=0)
+
+    return attn1
