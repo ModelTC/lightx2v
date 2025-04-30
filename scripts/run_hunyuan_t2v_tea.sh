e0b4cdc084f33835a26598c149c1317accdd98c1
Toryn <1584340372@qq.com>
2025-04-30 19:32:25 +0800

    function hunyuan_t2v_tea, hunyuan_t2v_taylorseer, modify the fresh_threshold of taylorseer

--- scripts/run_hunyuan_t2v_tea.sh
+++ scripts/run_hunyuan_t2v_tea.sh
@@ -26,12 +26,11 @@ export TOKENIZERS_PARALLELISM=false
 export PYTHONPATH=${lightx2v_path}:$PYTHONPATH
 
 export ENABLE_PROFILING_DEBUG=true
-export ENABLE_GRAPH_MODE=false
 
 python -m lightx2v.infer \
 --model_cls hunyuan \
 --task t2v \
 --model_path $model_path \
---config_json ${lightx2v_path}/configs/hunyuan_t2v.json \
+--config_json ${lightx2v_path}/configs/caching/hunyuan_t2v_Tea.json \
 --prompt "A cat walks on the grass, realistic style." \
---save_video_path ${lightx2v_path}/save_results/output_lightx2v_hy_t2v.mp4
+--save_video_path ${lightx2v_path}/save_results/output_lightx2v_hy_t2v_tea.mp4
