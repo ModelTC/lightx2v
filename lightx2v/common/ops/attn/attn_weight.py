f11256cae54f82bc623f4aa7fdfeab8bcadbeaec
Yang Yong(雍洋) <yongyang1030@163.com>
2025-05-13 14:21:30 +0800

    Update attn_weight.py

--- lightx2v/common/ops/attn/attn_weight.py
+++ lightx2v/common/ops/attn/attn_weight.py
@@ -53,11 +53,9 @@ class AttnWeightTemplate(metaclass=ABCMeta):
             self.config = config
 
     def to_cpu(self, non_blocking=False):
-        # self.weight = self.weight.to("cpu", non_blocking=non_blocking)
         pass
 
     def to_cuda(self, non_blocking=False):
-        # self.weight = self.weight.cuda(non_blocking=non_blocking)
         pass
 
     def state_dict(self, destination=None):
