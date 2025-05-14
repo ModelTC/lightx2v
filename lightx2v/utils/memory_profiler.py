f99ece42c0767c34f3b39fe858261b3a587f081b
“de1star” <843414674@qq.com>
2025-05-14 22:14:08 +0800

    fix offload

--- lightx2v/utils/memory_profiler.py
+++ lightx2v/utils/memory_profiler.py
@@ -13,17 +13,17 @@ def peak_memory_decorator(func):
         # 如果使用GPU，重置显存统计
         if torch.cuda.is_available():
             torch.cuda.reset_peak_memory_stats()
-        
+
         # 执行目标函数
         result = func(*args, **kwargs)
-        
+
         # 获取峰值显存
         if torch.cuda.is_available():
-            peak_memory = torch.cuda.max_memory_allocated() / (1024 ** 3)  # 转换为GB
+            peak_memory = torch.cuda.max_memory_allocated() / (1024**3)  # 转换为GB
             logger.info(f"{rank_info}Function '{func.__qualname__}' Peak Memory: {peak_memory:.2f} GB")
         else:
             logger.info(f"{rank_info}Function '{func.__qualname__}' executed without GPU.")
-        
+
         return result
-    
+
     return wrapper
