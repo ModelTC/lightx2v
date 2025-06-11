fc25210f432592ae2513d38fff715b6c54b90c0d
Toryn <1584340372@qq.com>
2025-06-11 14:05:01 +0800

    add decision list for WanScheduler

--- lightx2v/models/schedulers/wan/feature_caching/scheduler.py
+++ lightx2v/models/schedulers/wan/feature_caching/scheduler.py
@@ -3,13 +3,8 @@ from lightx2v.models.schedulers.wan.scheduler import WanScheduler
 
 # 1. TeaCaching: 对单步去噪的前半段，后半段分别缓存
 class WanSchedulerTeaCaching(WanScheduler):
-    # 1. 初始化
     def __init__(self, config):
-        # 1.1 初始化
         super().__init__(config)
-
-        # 1.2 多一个用于后半段的缓存决策
-        self.caching_records_2 = [True] * self.infer_steps
 
     def clear(self):
         # TODO
@@ -17,12 +12,9 @@ class WanSchedulerTeaCaching(WanScheduler):
 
 # 1. Taylor: 对单步去噪的前半段，后半段分别缓存
 class WanSchedulerTaylorCaching(WanScheduler):
-    # 1. 初始化
     def __init__(self, config):
-        # 1.1 初始化
         super().__init__(config)
 
-        # 1.2 多一个用于后半段的缓存决策
         pattern = [True, False, False, False]
         self.caching_records = (pattern * ((config.infer_steps + 3) // 4))[:config.infer_steps]
         self.caching_records_2 = (pattern * ((config.infer_steps + 3) // 4))[:config.infer_steps]
@@ -33,13 +25,8 @@ class WanSchedulerTaylorCaching(WanScheduler):
 
 # 1. AdaCaching: 对单步去噪的前半段，后半段分别缓存
 class WanSchedulerAdaCaching(WanScheduler):
-    # 1. 初始化
     def __init__(self, config):
-        # 1.1 初始化
         super().__init__(config)
-
-        # 1.2 多一个用于后半段的缓存决策
-        self.caching_records_2 = [True] * self.infer_steps
 
     def clear(self):
         # TODO
